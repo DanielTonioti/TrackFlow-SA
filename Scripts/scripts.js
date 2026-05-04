cab7499b719d500a1e9f9818617a6cf94e09ee73
Roberto <roberto_hille@estudante.sesisenai.org.br>
2026-05-04 11:05:41 -0300

    add - login scripts

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -2,6 +2,9 @@ let Contas = (("Marlon", "123"), ("Susane", "1234"), ("Frederico", "12345"))
 
 console.log(Contas[0][1]);
 
-document.getElementById("id001").innerHTML{
+document.getElementById("id001").addEventListener("submit", function (e){
 
-}
+let usuario = document.getElementById("id002").value;
+let senha = document.getElementById("id003").value;
+
+})
