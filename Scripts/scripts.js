e99f52d52f48c026b2ade125507bffc69f35488a
Roberto <roberto_hille@estudante.sesisenai.org.br>
2026-05-04 11:29:38 -0300

    add - input checks

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,4 +1,5 @@
 let Contas = (("Marlon", "123"), ("Susane", "1234"), ("Frederico", "12345"))
+let Cancel = 0;
 
 console.log(Contas[0][1]);
 
@@ -6,5 +7,16 @@ document.getElementById("id001").addEventListener("submit", function (e){
 
 let usuario = document.getElementById("id002").value;
 let senha = document.getElementById("id003").value;
+
+if (usuario == null)
+{
+    cancel = 1;
+}
+if (senha.length < 4)
+{
+    cancel = 1;
+}
+
+
 
 })
