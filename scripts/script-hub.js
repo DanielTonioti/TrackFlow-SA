86e7548f5ba02c0473d25ca0492964890a84dff1
Roberto <roberto_hille@estudante.sesisenai.org.br>
2026-05-07 21:50:34 -0300

    fix - """"performance""""

--- scripts/script-hub.js
+++ scripts/script-hub.js
@@ -1,44 +1,40 @@
-let time = 120;
-/* let checker = localStorage.getItem(usuario, senha);
-if (checker !== senha){
-window.location.href = "../public/login.html";
-} */
+let time = 30;
 
 document.getElementById("hub-toggle-admin").onclick = () => {
-    time = 120;
+    time = 30;
     let id001 = setInterval(function () {
         if (time >= 1) {
             time -= 1;
         }
         else {
             window.location.href = "admin.html";
-            time = 120;
+            time = 30;
             clearInterval(id001);
         }
     }, 10);
 }
 document.getElementById("hub-toggle-relatories").onclick = () => {
-    time = 120;
+    time = 30;
     let id001 = setInterval(function () {
         if (time >= 1) {
             time -= 1;
         }
         else {
             window.location.href = "relatories.html";
-            time = 120;
+            time = 30;
             clearInterval(id001);
         }
     }, 10);
 }
 document.getElementById("hub-toggle-sensor").onclick = () => {
-    time = 120;
+    time = 30;
     let id001 = setInterval(function () {
         if (time >= 1) {
             time -= 1;
         }
         else {
             window.location.href = "sensor.html";
-            time = 120;
+            time = 30;
             clearInterval(id001);
         }
     }, 10);
--- scripts/script-index.js
+++ scripts/script-index.js
@@ -1,13 +1,13 @@
-let time = 120;
+let time = 30;
 document.getElementById("index-button").onclick = () => {
-    time = 120;
+    time = 30;
     let id001 = setInterval(function () {
         if (time >= 1) {
             time -= 1;
         }
         else {
             window.location.href = "public/login.html";
-            time = 120;
+            time = 30;
             clearInterval(id001);
         }
     }, 10);
