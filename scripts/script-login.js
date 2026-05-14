42ba7a031bd1c4e3d28cb41e6ce8d041d3cd173a
DanielTonioti <daniel_tonioti@estudante.sesisenai.org.br>
2026-05-14 18:22:21 -0300

    UP - Now the admin page is users page and the users are contained in a list, needing a button to redirect to user info

--- scripts/script-login.js
+++ scripts/script-login.js
@@ -1,36 +1,32 @@
-let contas = (("marlon@gmail.com", "1234"), 
-    ("susane@gmail.com", "1234"), 
+let contas = (("marlon@gmail.com", "1234"),
+    ("susane@gmail.com", "1234"),
     ("frederico@gmail.com", "1234")
 ); // Small test at pre-inserted accounts
 let cancel = 0; // Cancels login
 
-document.getElementById("form-login").addEventListener("submit", function (e){
-e.preventDefault();
+document.getElementById("form-login").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-cancel = 0;
-let usuario = document.getElementById("usuario-email").value;
-let senha = document.getElementById("usuario-senha").value;
+    cancel = 0;
+    let usuario = document.getElementById("usuario-email").value;
+    let senha = document.getElementById("usuario-senha").value;
 
-if (usuario == null)
-{
-    cancel = 1;
-}
-if (!usuario.includes("@"))
-{
-    cancel = 1;
-}
-if (senha.length < 4)
-{
-    cancel = 1;
-}
-if (cancel == 1)
-{
-alert("Dados Incorretos");
-}
-if (cancel == 0){
-localStorage.setItem(usuario, senha);
-window.location.href = "../public/hub.html";
-}
+    if (usuario == null) {
+        cancel = 1;
+    }
+    if (!usuario.includes("@")) {
+        cancel = 1;
+    }
+    if (senha.length < 4) {
+        cancel = 1;
+    }
+    if (cancel == 1) {
+        alert("Dados Incorretos");
+    }
+    if (cancel == 0) {
+        localStorage.setItem(usuario, senha);
+        window.location.href = "../public/hub.html";
+    }
 
 }) // This will allow anyone to log-in, only checking requirements inside the input. This will be connected to the database later.
 
